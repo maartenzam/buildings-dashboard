c44170ebcc74c650d64409fb26c71c548badd893
Maarten <maartenzam@gmail.com>
2021-05-21 10:32:34 +0200

    Basic country page outline

--- src/routes/CountryPage.svelte
+++ src/routes/CountryPage.svelte
@@ -3,4 +3,41 @@
   const route = meta();
 </script>
 
-<div>Country: {route.params.country}</div>
+<div class="wrapper">
+  <div class="row">
+    <div class="cell">Country: {route.params.country}</div>
+    <div class="cell">Test</div>
+    <div class="cell">Test</div>
+  </div>
+  <div class="row">
+    <div class="cell">Test</div>
+    <div class="cell">Test</div>
+    <div class="cell">Test</div>
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    flex: 1;
+  }
+  .row {
+    display: flex;
+    height: 50%;
+    flex-wrap: wrap;
+  }
+  .cell {
+    flex: 1;
+    background-color: "salmon";
+    min-width: 300px;
+  }
+
+  @media (max-width: 900px) {
+    .row {
+      display: block;
+    }
+    .cell {
+      width: 100%;
+      margin: 0 0 10px 0;
+    }
+  }
+</style>
