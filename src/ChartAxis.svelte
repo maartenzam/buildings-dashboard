a710432109a2df654b30a36c2d172025fb53bcec
Maarten <maartenzam@gmail.com>
2021-05-25 15:24:06 +0200

    Axes

---
+++ src/ChartAxis.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let axisType = "xAxis";
+  export let translate = "translate(0, 0)";
+  export let x = 0;
+  export let x1 = 0;
+  export let x2 = 0;
+  export let y = 4;
+  export let y1 = 0;
+  export let y2 = 0;
+  export let text = "";
+</script>
+
+{#if axisType == "xAxis"}
+  <g class="tick" transform={translate}>
+    <line {y1} {y2} />
+    <text {y} style="text-anchor:middle;">{text}</text>
+  </g>
+{:else}
+  <g class="tick" transform={translate}>
+    <line {x1} {x2} />
+    <text {x} {y} style="text-anchor:end;">{text}</text>
+  </g>
+{/if}
+
+<style>
+  .tick line {
+    stroke: #a3a3a3;
+    stroke-dasharray: 2;
+    opacity: 0.6;
+  }
+
+  .tick text {
+    fill: #a3a3a3;
+    font-size: 11px;
+  }
+</style>
--- src/TrendChart.svelte
+++ src/TrendChart.svelte
@@ -1,15 +1,18 @@
 <script>
   import { scaleLinear, scaleTime } from "d3-scale";
   import { extent, min, max } from "d3-array";
+  import ChartAxis from "./ChartAxis.svelte";
+  import { timeFormat } from "d3-time-format";
+  import { format } from "d3-format";
 
   export let countryData;
   export let width;
   export let height;
   export let displayUnits;
 
-  const margins = { top: 10, left: 10, right: 10, bottom: 10 };
+  const margins = { top: 10, left: 30, right: 10, bottom: 10 };
 
-  const yAxisMargin = 0.2;
+  const yAxisMargin = 0;
 
   $: countryDataPoints = countryData[1];
 
@@ -28,11 +31,42 @@
 
   $: xScale = scaleTime().domain(xDomain).range([0, chartWidth]);
   $: yScale = scaleLinear().domain(yDomain).range([chartHeight, 0]);
+
+  const xTicks = [new Date(2005, 1), new Date(2010, 1), new Date(2015, 1)];
+  const formatTime = timeFormat("%y");
+
+  $: yTicks = yScale.ticks(3);
+  const formatNumber = format(".2s");
 </script>
 
 <svg {width} {height}>
   <rect {width} {height} fill={"#1db6c1"} opacity={0.2} />
   <g transform={`translate(${margins.left}, ${margins.top})`}>
+    <g class="axis y-axis">
+      {#each yTicks as tick}
+        <ChartAxis
+          axisType="yAxis"
+          translate="translate(0, {yScale(tick)})"
+          x1={xScale(xDomain[0])}
+          x2={xScale(xDomain[1])}
+          x={-4}
+          y="+4"
+          text={formatNumber(tick)}
+        />
+      {/each}
+    </g>
+    <g class="axis x-axis">
+      {#each xTicks as tick}
+        <ChartAxis
+          axisType="xAxis"
+          translate="translate({xScale(tick)},0)"
+          y1={yScale(yDomain[0])}
+          y2={yScale(yDomain[1])}
+          y={height - margins.bottom}
+          text={formatTime(tick)}
+        />
+      {/each}
+    </g>
     {#each countryDataPoints as point}
       <circle cx={xScale(point.time)} cy={yScale(point[displayUnits])} r={4} />
     {/each}
