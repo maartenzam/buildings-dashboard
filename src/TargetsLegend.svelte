401203b7a01df25142e6e2bf1da3c11e954a1b5e
Maarten <maartenzam@gmail.com>
2021-06-04 14:23:16 +0200

    Targets

--- src/TargetsLegend.svelte
+++ src/TargetsLegend.svelte
@@ -3,7 +3,7 @@
   export let height = 0;
   export let selectedIndicator;
 
-  const margins = { top: 10, left: 10, right: 10, bottom: 10 };
+  const margins = { top: 20, left: 10, right: 10, bottom: 10 };
   $: legendHeight = height - margins.top - margins.bottom;
 </script>
 
@@ -11,60 +11,58 @@
   <rect {width} {height} class="background" />
   <g class="target-lines">
     {#if selectedIndicator.indicatorCode === "fec"}
-      <line
-        class="target-2020"
-        x1={margins.left}
-        x2={width - margins.right}
-        y1={margins.top + legendHeight / 3}
-        y2={margins.top + legendHeight / 3}
-      />
-      <text
-        x={width / 2}
-        y={margins.top + legendHeight / 3}
-        dy={-4}
-        class="target-2020">2020 target</text
-      >
-      <line
-        class="target-necp"
-        x1={margins.left}
-        x2={width - margins.right}
-        y1={margins.top + (legendHeight / 3) * 2}
-        y2={margins.top + (legendHeight / 3) * 2}
-      />
-      <text
-        x={width / 2}
-        y={margins.top + (legendHeight / 3) * 2}
-        dy={-4}
-        class="target-necp">2030 contribution</text
-      >
-      <line
-        class="target-euco"
-        x1={margins.left}
-        x2={width - margins.right}
-        y1={margins.top + (legendHeight / 3) * 3}
-        y2={margins.top + (legendHeight / 3) * 3}
-      />
-      <text
-        x={width / 2}
-        y={margins.top + (legendHeight / 3) * 3}
-        dy={-4}
-        class="target-euco">2030 projection</text
-      >
+      <g transform="translate(0,0)">
+        <line
+          class="target-2020"
+          x1={margins.left}
+          x2={width - margins.right}
+          y1={margins.top}
+          y2={margins.top}
+        />
+        <text x={width / 2} y={margins.top} dy={-4} class="target-2020"
+          >2020 target</text
+        >
+      </g>
+      <g transform={`translate(0,${height / 2 + 4})`}>
+        <line
+          class="target-necp"
+          x1={margins.left}
+          x2={width - margins.right}
+          y1={0}
+          y2={0}
+        />
+        <text x={width / 2} y={0} dy={-4} class="target-necp"
+          >2030 national</text
+        >
+        <text x={width / 2} y={0} dy={11} class="target-necp">contribution</text
+        >
+      </g>
+      <g transform={`translate(0,${height - margins.bottom})`}>
+        <line
+          class="target-euco"
+          x1={margins.left}
+          x2={width - margins.right}
+          y1={0}
+          y2={0}
+        />
+        <text x={width / 2} y={0} dy={-4} class="target-euco"
+          >2030 EU projection</text
+        >
+      </g>
     {/if}
     {#if selectedIndicator.indicatorCode === "fechh"}
-      <line
-        class="target-euco"
-        x1={margins.left}
-        x2={width - margins.right}
-        y1={margins.top + (legendHeight / 3) * 2}
-        y2={margins.top + (legendHeight / 3) * 2}
-      />
-      <text
-        x={width / 2}
-        y={margins.top + (legendHeight / 3) * 2}
-        dy={-4}
-        class="target-euco">2030 projection</text
-      >
+      <g transform={`translate(0,${height - margins.bottom})`}>
+        <line
+          class="target-euco"
+          x1={margins.left}
+          x2={width - margins.right}
+          y1={0}
+          y2={0}
+        />
+        <text x={width / 2} y={0} dy={-4} class="target-euco"
+          >2030 EU projection</text
+        >
+      </g>
     {/if}
   </g>
 </svg>
