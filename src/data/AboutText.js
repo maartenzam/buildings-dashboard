c62a97eab965a3d7f2c0a6d4e8c8b317bd170c4e
Maarten <maartenzam@gmail.com>
2021-06-23 09:42:18 +0200

    About text, Excel update

--- src/data/AboutText.js
+++ src/data/AboutText.js
@@ -1,10 +1,29 @@
 const AboutText = [
-  `This dashboard monitors progress of buildings across the EU 27 Member States towards the 2050 net-zero GHG emissions goal. It has a focus on the structural drivers of GHG emissions in residential buildings, which represent 75% of the buildings stock.`,
-  `The dashboard shows the development of key energy and policy parameters since 2005 and will be regularly updated over the coming years. This should normally happen in January, when Eurostat publishes new energy data.`,
-  `Data on buildings are scarce. Those on renovation rates and depths are not systematically collected. The latest figures stem from a <a href="https://ec.europa.eu/energy/sites/ener/files/documents/1.final_report.pdf"></a>report published by the European Commission in 2019 and are based on a survey reflecting the situation by 2016. What about energy performance certificates? Not available as well – they are neither standardised nor are data collected at national or EU level.`,
-  `Therefore, final energy consumption (FEC) data are the central parameter of this dashboard, as they are closely linked to the EU’s energy efficiency target and the energy efficiency of buildings and thus the overall energy performance of buildings. Data on energy use are based on market data. They are collected systematically, are robust and regularly updated. FEC data, both total and in households, are complemented by a set of additional indicators (quantitative and qualitative ones) which allow to draw a detailed picture of the situation and progress in the sector. The data are lagging by roughly two years, the delay of publication by Eurostat. Qualitative indicators are based on studies or assessments by the European Commission or credible players in the sector.`,
-  `In order to better <b>visualise general trends</b>, the charts are produced using <a href="https://en.wikipedia.org/wiki/Local_regression">local regression</a>, a method used to smoothen variability of data in the time series.`,
-  `All raw data used to produce the dashboard <a href="/ECF-buildings-dashboard-data.xlsx" tinro-ignore download>can be downloaded here.</a>`,
+  `
+  The <b>Dashboard on progress of Europe’s buildings towards climate neutrality</b> monitors progress of buildings across the EU 27 Member States towards the 2050 net-zero GHG emissions goal. It focuses on the structural drivers of GHG emissions in residential buildings, which represent 75% of the European buildings stock.
+  `,
+  `
+  The dashboard shows the development of key energy parameters since 2005 and will be regularly updated over the coming years. This should normally happen in January, when Eurostat publishes new energy data. Policy indicators will be updated as new information becomes available.
+  `,
+  `
+  <b>Data on buildings are scarce</b>. Those on renovation rates and depths are not systematically collected. The latest figures stem from a <a href="https://ec.europa.eu/energy/sites/ener/files/documents/2.annex_to_final_report.pdf" target="_blank">report</a> published by the European Commission in 2019 and are based on a survey reflecting the situation in 2016. Data from energy performance certificates are neither standardised nor collected at national or EU level.
+  `,
+  `
+  Therefore, <b>final energy consumption (FEC)</b> data are the central parameter of this dashboard, as they are closely linked to the EU’s energy efficiency target and the energy performance of buildings. Data on energy use are based on market data. They are collected systematically, robust and regularly updated. The FEC data are complemented by a set of additional qualitative and quantitative indicators which helps to draw a detailed picture of the situation and progress on the ground. The data are lagging by around two years, the delay of publication by Eurostat. Qualitative indicators are based on studies or assessments by the European Commission or other credible players in the sector.
+  `,
+  `<u>How to use this dashboard?</u>`,
+  `
+  The <a href="/">overview</a> displays the EU and individual 27 Member States data, arranged in an adapted geographical order, indicator by indicator - to be selected in the <b>left-hand drop-down menu</b>. The overview provides the definition and source of the indicator, as well as additional information. Some indicators can be displayed as absolute values or put in relation to other factors, such as GDP, population or climate - to be selected in the <b>right-hand drop-down menu</b>.
+  `,
+  `The <a href="/country/EU27_2020">country pages</a> display all indicators for one country at a glance and are accessed by <b>clicking the name</b> of the desired Member State. <b>Clicking the graph</b> will take you to an enlarged view of the graph. You can <b>switch from one country page to another</b> via the drop-down menu on top of the country page.
+  `,
+  `In order to better visualise general trends, the charts are produced using local regression, a method used to smoothen variability of data in the time series.
+  `,
+  `All raw data used to produce the dashboard <a href="ECF-buildings-dashboard-data.xlsx" download>can be downloaded here</a>.
+  `,
+  `<u>Imprint</u>`,
+  `The <b>Dashboard on progress of Europe’s buildings towards climate neutrality</b> is developed by <a href="https://www.stefanscheuer.eu/" target="_blank">Stefan Scheuer Consulting</a> and <a href="https://www.maartenlambrechts.com" target="_blank">Maarten Lambrechts</a> for the <a href="https://europeanclimate.org/" target="_blank">European Climate Foundation</a> and published in June 2021.
+  `,
 ];
 
 export default AboutText;
