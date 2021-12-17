ad20e000fd6e7c1bde222fcc1b385d2d38b836e9
Maarten <maartenzam@gmail.com>
2021-12-17 13:16:16 +0100

    Testing

--- src/data/AboutText.js
+++ src/data/AboutText.js
@@ -22,16 +22,15 @@ const AboutText = [
   `All raw data used to produce the dashboard <a href="ECF-buildings-dashboard-data.xlsx" download>can be downloaded here</a>.
   `,
   `<u>Imprint</u>`,
-  `The <b>Dashboard on progress of Europe’s buildings towards climate neutrality</b> is developed by <a href="https://www.stefanscheuer.eu/" target="_blank">Stefan Scheuer Consulting</a> and <a href="https://www.maartenlambrechts.com" target="_blank">Maarten Lambrechts</a> for the <a href="https://europeanclimate.org/" target="_blank">European Climate Foundation</a> and published in June 2021.
-  `
+  `The <b>Dashboard on progress of Europe’s buildings towards climate neutrality</b> is developed by <a href="https://www.stefanscheuer.eu/" target="_blank">Stefan Scheuer Consulting</a> and <a href="https://www.maartenlambrechts.com" target="_blank">Maarten Lambrechts</a> for the <a href="https://europeanclimate.org/" target="_blank">European Climate Foundation</a> and published in June 2021. What?
+  `,
   `<u>Contact for feedback and questions:</u>
-  `
+  `,
   `Almut Bonhage
-  `Stefan Scheuer Consulting
-  `almut.bonhage@stefanscheuer.eu
-  `T +32 2 235 20 12
-  `M +32 474 76 22 74  
-  ,
+  Stefan Scheuer Consulting
+  almut.bonhage@stefanscheuer.eu
+  T +32 2 235 20 12
+  M +32 474 76 22 74`,
 ];
 
 export default AboutText;
