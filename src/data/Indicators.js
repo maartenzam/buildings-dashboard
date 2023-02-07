2d304e0d08095f727830be23fc7218a9ec55fcea
Maarten <maartenzam@gmail.com>
2023-02-07 11:37:33 +0100

    Eurostat link updates

--- src/data/Indicators.js
+++ src/data/Indicators.js
@@ -20,11 +20,11 @@ const indicators = [
     indicatorSources: [
       {
         text: "Eurostat FEC (Europe 2020-2030) [nrg_bal_c]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-1015839_QID_-24AF4327_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;NRG_BAL,L,Z,0;SIEC,L,Z,1;UNIT,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-1015839INDICATORS,OBS_FLAG;DS-1015839UNIT,KTOE;DS-1015839NRG_BAL,FEC2020-2030;DS-1015839SIEC,TOTAL;&rankName1=SIEC_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=NRG-BAL_1_2_-1_2&rankName5=TIME_1_0_0_0&rankName6=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NRG_BAL_C__custom_4815050/default/table?lang=en",
       },
       {
         text: "Eurostat GDP [nama_10_gdp]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-406763_QID_21D2AEB1_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;UNIT,L,Z,0;NA_ITEM,L,Z,1;INDICATORS,C,Z,2;&zSelection=DS-406763INDICATORS,OBS_FLAG;DS-406763UNIT,CLV10_MEUR;DS-406763NA_ITEM,B1GQ;&rankName1=NA-ITEM_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=TIME_1_0_0_0&rankName5=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NAMA_10_GDP__custom_4859392/default/table?lang=en",
       },
     ],
     treeMapTitle: "FEC (Mtoe) by Members State",
@@ -53,19 +53,19 @@ const indicators = [
     indicatorSources: [
       {
         text: "Eurostat FEC in households [nrg_bal_c]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-1015839_QID_-11C28DC8_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;NRG_BAL,L,Z,0;SIEC,L,Z,1;UNIT,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-1015839INDICATORS,OBS_FLAG;DS-1015839UNIT,KTOE;DS-1015839NRG_BAL,FEC2020-2030;DS-1015839SIEC,TOTAL;&rankName1=SIEC_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=NRG-BAL_1_2_-1_2&rankName5=TIME_1_0_0_0&rankName6=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NRG_BAL_C__custom_4815147/default/table?lang=en",
       },
       {
         text: "Eurostat Cooling and heating degree days [nrg_chdd_a]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=nrg_chdd_a&lang=en",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NRG_CHDD_A__custom_4859484/default/table?lang=en",
       },
       {
         text: "Eurostat Disaggregated FEC in households [nrg_d_hhq]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-1167021_QID_-2FA34126_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;NRG_BAL,L,Z,0;SIEC,L,Z,1;UNIT,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-1167021NRG_BAL,FC_OTH_HH_E;DS-1167021INDICATORS,OBS_FLAG;DS-1167021SIEC,TOTAL;DS-1167021UNIT,TJ;&rankName1=SIEC_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=NRG-BAL_1_2_-1_2&rankName5=TIME_1_0_0_0&rankName6=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NRG_D_HHQ__custom_4859653/default/table?lang=en",
       },
       {
         text: "Eurostat Population on 1 January [demo_pjan]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=demo_pjan&lang=en",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/DEMO_PJAN__custom_4859748/default/table?lang=en",
       },
     ],
     treeMapTitle: "FEC (Mtoe) in households by Members State",
@@ -88,7 +88,7 @@ const indicators = [
     indicatorSources: [
       {
         text: "Eurostat Complete energy balances [nrg_bal_c]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-1015839_QID_-5D358863_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;NRG_BAL,L,Z,0;SIEC,L,Z,1;UNIT,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-1015839INDICATORS,OBS_FLAG;DS-1015839UNIT,KTOE;DS-1015839NRG_BAL,FC_OTH_HH_E;DS-1015839SIEC,RA000;&rankName1=SIEC_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=NRG-BAL_1_2_-1_2&rankName5=TIME_1_0_0_0&rankName6=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/NRG_BAL_C__custom_4859904/default/table?lang=en",
       },
     ],
   },
@@ -105,7 +105,7 @@ const indicators = [
     indicatorSources: [
       {
         text: "Eurostat EU-SILC Total population living in a dwelling with a leaking roof, damp walls, floors or foundation, or rot in window frames or floor [ilc_mdho01]",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-056148_QID_77D5F155_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;HHTYP,L,Z,0;INCGRP,L,Z,1;UNIT,L,Z,2;SEX,L,Z,3;AGE,L,Z,4;INDICATORS,C,Z,5;&zSelection=DS-056148HHTYP,TOTAL;DS-056148SEX,T;DS-056148UNIT,PC;DS-056148INCGRP,TOTAL;DS-056148AGE,TOTAL;DS-056148INDICATORS,OBS_FLAG;&rankName1=HHTYP_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=SEX_1_2_-1_2&rankName5=INCGRP_1_2_-1_2&rankName6=AGE_1_2_-1_2&rankName7=TIME_1_0_0_0&rankName8=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=NONE&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/ILC_MDHO01__custom_4815309/default/table?lang=en",
       },
     ],
   },
@@ -124,7 +124,7 @@ const indicators = [
     indicatorSources: [
       {
         text: "Eurostat EU-SILC Population unable to keep home adequately warm by poverty status, total [sdg_07_60] ",
-        url: "https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-869738_QID_604DFE40_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;HHTYP,L,Z,0;INCGRP,L,Z,1;UNIT,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-869738INCGRP,A_MD60;DS-869738INDICATORS,OBS_FLAG;DS-869738UNIT,PC;DS-869738HHTYP,TOTAL;&rankName1=HHTYP_1_2_-1_2&rankName2=UNIT_1_2_-1_2&rankName3=INDICATORS_1_2_-1_2&rankName4=INCGRP_1_2_-1_2&rankName5=TIME_1_0_0_0&rankName6=GEO_1_2_0_1&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=NONE&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23",
+        url: "https://ec.europa.eu/eurostat/databrowser/view/SDG_07_60__custom_4815463/default/table?lang=en",
       },
     ],
   },
